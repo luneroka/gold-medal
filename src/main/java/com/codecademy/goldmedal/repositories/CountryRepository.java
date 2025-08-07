841d2c692dcdc58a1d2a081f7495b8a85ba60f76
Luneroka <luneroka@gmail.com>
2025-08-07 21:56:30 +0200

    Complete CountryRepository methods

--- src/main/java/com/codecademy/goldmedal/repositories/CountryRepository.java
+++ src/main/java/com/codecademy/goldmedal/repositories/CountryRepository.java
@@ -3,8 +3,15 @@ package com.codecademy.goldmedal.repositories;
 import com.codecademy.goldmedal.model.Country;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CountryRepository extends CrudRepository<Country, Integer> {
   Optional<Country> findCountryByName(String countryName);
+  List<Country> getAllByOrderByNameAsc();
+  List<Country> getAllByOrderByNameDesc();
+  List<Country> getAllByOrderByGdpAsc();
+  List<Country> getAllByOrderByGdpDesc();
+  List<Country> getAllByOrderByPopulationAsc();
+  List<Country> getAllByOrderByPopulationDesc();
 }
